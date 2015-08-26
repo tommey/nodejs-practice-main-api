64e434355fc6463831a9df3735a36674e5d9dc45
Tamas Toth <tommey@tommey.hu>
2015-08-26 08:13:29 -0400

    Added loop for requests to see the circuit breaker work and reset after the service is available again

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ var request = require('request');
 var circuitBreakerOptions = {
 	maxFailures: 5,
 	timeout: 300,
-	resetTimeout: 30000
+	resetTimeout: 3000
 };
 
 var circuit = levee.createBreaker(request.get, circuitBreakerOptions);
 
-circuit.run('http://localhost:81/users', function(err, data, body) {
-	if (err) return console.error(err);
+function makeRequest() {
+	circuit.run('http://localhost:81/users', function(err, data, body) {
+		if (err) return console.error(err);
 
-	console.log(body);
-});
+		console.log(body);
+	});
+}
+
+setInterval(makeRequest, 1000);
