614e63db76a5385a2ce0873d3b8d9864903ff541
Tamas Toth <tommey@tommey.hu>
2015-08-26 08:05:43 -0400

    Added circuit breaker for request.get

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var levee = require('levee');
 var request = require('request');
 
-request.get('http://localhost:81/users', function(err, data) {
-	console.log(err || data.body);
+var circuitBreakerOptions = {
+	maxFailures: 5,
+	timeout: 300,
+	resetTimeout: 30000
+};
+
+var circuit = levee.createBreaker(request.get, circuitBreakerOptions);
+
+circuit.run('http://localhost:81/users', function(err, data, body) {
+	if (err) return console.error(err);
+
+	console.log(body);
 });
