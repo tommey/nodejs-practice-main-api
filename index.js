5f7da9c0c481e79fbbcb11c2076094c06de56700
Tamas Toth <tommey@tommey.hu>
2015-08-26 09:08:49 -0400

    Added feature to parallel call the products and users api-s and return the list of the entities with circuit breaker support and separate connection pools

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+var koa = require('koa');
+var http = require('http');
+
 var levee = require('levee');
 var request = require('request');
+
+var config = require('./config');
+
+var app = koa();
 
 var circuitBreakerOptions = {
 	maxFailures: 5,
@@ -7,14 +14,41 @@ var circuitBreakerOptions = {
 	resetTimeout: 3000
 };
 
-var circuit = levee.createBreaker(request.get, circuitBreakerOptions);
+var userApiPool = new http.Agent();
+var productApiPool = new http.Agent();
 
-function makeRequest() {
-	circuit.run('http://localhost:81/users', function(err, data, body) {
-		if (err) return console.error(err);
+function createClient(connectionPool, url) {
+	var circuit = levee.createBreaker(request.get, circuitBreakerOptions);
 
-		console.log(body);
+	return new Promise(function(resolve, reject) {
+		circuit.run({
+			agent: connectionPool, 
+			url: url
+		}, function(err, data, body) {
+			if (err) return reject(err);
+
+			resolve(JSON.parse(body));
+		});
 	});
 }
 
-setInterval(makeRequest, 1000);
+app.use(function* (next) {
+	var start = new Date();
+        console.log('Request', this.method, this.path, start);
+	yield next;
+	console.log('Request processed in', (new Date()).getTime() - start.getTime(), 'ms');
+});
+
+app.use(function* () {
+	this.body = yield {
+		users: createClient(userApiPool, 'http://localhost:81/users'),
+		products: createClient(productApiPool, 'http://localhost:82/products')
+	};
+});
+
+app.listen(config.port, function(err) {
+        if (err) throw err;
+
+        console.log('Server is listening on port', config.port);
+});
+
