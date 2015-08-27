da609039dc2f58648c5826e19d03ed94081248bc
Tamas Toth <tommey@tommey.hu>
2015-08-27 07:32:13 -0400

    Moved index.js to the src directory and exported the app, so the app.listen is in another file, which makes testing more possible

--- index.js
+++ index.js
@@ -1,54 +1,9 @@
-var koa = require('koa');
-var http = require('http');
-
-var levee = require('levee');
-var request = require('request');
-
+var app = require('./src');
 var config = require('./config');
 
-var app = koa();
-
-var circuitBreakerOptions = {
-	maxFailures: 5,
-	timeout: 300,
-	resetTimeout: 3000
-};
-
-var userApiPool = new http.Agent();
-var productApiPool = new http.Agent();
-
-function createClient(connectionPool, url) {
-	var circuit = levee.createBreaker(request.get, circuitBreakerOptions);
-
-	return new Promise(function(resolve, reject) {
-		circuit.run({
-			agent: connectionPool, 
-			url: url
-		}, function(err, data, body) {
-			if (err) return reject(err);
-
-			resolve(JSON.parse(body));
-		});
-	});
-}
-
-app.use(function* (next) {
-	var start = new Date();
-        console.log('Request', this.method, this.path, start);
-	yield next;
-	console.log('Request processed in', (new Date()).getTime() - start.getTime(), 'ms');
-});
-
-app.use(function* () {
-	this.body = yield {
-		users: createClient(userApiPool, 'http://localhost:81/users'),
-		products: createClient(productApiPool, 'http://localhost:82/products')
-	};
-});
-
 app.listen(config.port, function(err) {
-        if (err) throw err;
+	if (err) throw err;
 
-        console.log('Server is listening on port', config.port);
+	console.log('Server is listening on port', config.port);
 });
 
---
+++ src/index.js
@@ -0,0 +1,49 @@
+var koa = require('koa');
+var http = require('http');
+
+var levee = require('levee');
+var request = require('request');
+
+var config = require('../config');
+
+var app = koa();
+
+var circuitBreakerOptions = {
+	maxFailures: 5,
+	timeout: 300,
+	resetTimeout: 3000
+};
+
+var userApiPool = new http.Agent();
+var productApiPool = new http.Agent();
+
+function createClient(connectionPool, url) {
+	var circuit = levee.createBreaker(request.get, circuitBreakerOptions);
+
+	return new Promise(function(resolve, reject) {
+		circuit.run({
+			agent: connectionPool, 
+			url: url
+		}, function(err, data, body) {
+			if (err) return reject(err);
+
+			resolve(JSON.parse(body));
+		});
+	});
+}
+
+app.use(function* (next) {
+	var start = new Date();
+        console.log('Request', this.method, this.path, start);
+	yield next;
+	console.log('Request processed in', (new Date()).getTime() - start.getTime(), 'ms');
+});
+
+app.use(function* () {
+	this.body = yield {
+		users: createClient(userApiPool, 'http://localhost:81/users'),
+		products: createClient(productApiPool, 'http://localhost:82/products')
+	};
+});
+
+module.exports = app;
