7bccc8b2978120902fd157a31df3026a2f4c560c
Tamas Toth <tommey@tommey.hu>
2015-08-27 08:02:26 -0400

    Added nock for the users and products API-s, so the http calls are faked

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,16 +1,24 @@
 var http = require('http');
 var nock = require('nock');
 var supertest = require('supertest');
+var expect = require('chai').expect;
 
 var app = http.createServer(require('./').callback());
 
 describe('The Main API', function(){
 	it ('responds with the correct body and status code', function(done){
+		var users = [{name: 'John', age: 32}];
+		var products = [{name: 'Thing', color: 'Red', price: 32}];
+		var list = {users: users, products: products};
+		
+		nock('http://localhost:81').get('/users').reply(200, users);
+		nock('http://localhost:82').get('/products').reply(200, products);
+
 		supertest(app)
 			.get('/')
 			.expect(200)
 			.end(function(err, response){
-				console.log(response.body);
+				expect(response.body).to.eql(list);
 				done(err);
 			});
 	});
